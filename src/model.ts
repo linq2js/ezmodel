b59dabaabb4ed0dee778c1cd191ba7b6cd4817a7
ginguyen <ginguyen@axon.com>
2024-02-13 14:10:30 +0700

    chore: script

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,5 @@
 import { getValue, setValue } from "./alter";
-import { all, async } from "./async";
+import { async } from "./async";
 import { disposable } from "./disposable";
 import { emitter } from "./emitter";
 import { local } from "./local";
