bb024f906cff87965f7104e12adedbf6415666df
ginguyen <ginguyen@axon.com>
2024-02-24 12:03:05 +0700

    refactor: improve rx

--- src/react/rx.ts
+++ src/react/rx.ts
@@ -1,17 +1,46 @@
-import { ReactElement, createElement, memo, useRef } from "react";
+import { ReactElement, createElement, memo, useState } from "react";
 import { AnyFunc } from "../types";
 import { trackable } from "..";
 import { useRerender } from "./useRerender";
+import { NOOP } from "~/utils";
 
-const Part = memo((props: { fn: AnyFunc }) => {
+const Part = memo(({ fn }: { fn: AnyFunc }): any => {
   const rerender = useRerender();
-  const untrackRef = useRef<VoidFunction>();
+  const ref = useState(() => ({
+    fn: NOOP as AnyFunc,
+    untrack: NOOP,
+    error: undefined as any,
+    result: undefined as any,
+  }))[0];
 
-  untrackRef.current?.();
-  const [{ track }, result] = trackable(props.fn);
-  untrackRef.current = track(rerender);
+  if (ref.fn !== fn) {
+    ref.untrack();
+    ref.fn = fn;
+    let currentRererender = NOOP;
+    const handleChange = () => {
+      try {
+        ref.result = fn();
+      } catch (ex) {
+        ref.error = ex;
+      }
+      currentRererender();
+    };
+    const { track } = trackable(handleChange)[0];
+    ref.untrack = track(() => {
+      currentRererender = rerender;
+      handleChange();
+    });
+  }
 
-  return result;
+  if (ref.error) {
+    ref.untrack();
+    ref.fn = NOOP;
+    const error = ref.error;
+    ref.error = undefined;
+    throw error;
+  }
+
+  return ref.result;
 });
 
 export const rx = <T>(fn: () => T): ReactElement => {
